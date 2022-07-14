8aa96be87877291406b21ccc7d97ff11ee135736
Rossi Kamal <rossikamal@gmail.com>
2022-07-15 05:14:01 +0600

    Update Feature.js

--- components/Feature.js
+++ components/Feature.js
@@ -40,7 +40,7 @@ const FeatureBox = (props) => {
           <div>
             <img src={feature.img} alt="" className="img-fluid d-block mx-auto"/>
           </div>
-        </Col>ttttttttttt
+        </Col>
       </Row>
       )
     }
@@ -51,9 +51,9 @@ const FeatureBox = (props) => {
 const Feature = () => {
 
   const features = [
-    {id : 1, img : "./images/45.png", title : "Banking", desc : "Enjoy banking from premius corporate houses", link : "/"},
-    {id : 2, img : "./images/Group Members.png", title : "Utility", desc : "Pay Electricity, Gas and Education Fee", link : "/"},
-    {id : 3, img : "./images/45.png", title : "E-Commerce", desc : "Buy products online with eWallet", link : "/"},
+    {id : 1, img : "./images/45.png", title : "LOREM IPSUM", desc : "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", link : "/"},
+    {id : 2, img : "./images/Group Members.png", title : "LOREM IPSUM", desc : "Sed perspiciatis unde omnis natus error voluptatem accusantium doloremque laudantium totam rem aperiam eaque ipsa quae ab illo excepturi sint occaecati cupiditate architecto.", link : "/"},
+    {id : 3, img : "./images/45.png", title : "LOREM IPSUM", desc : "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", link : "/"},
   ];
 
   return (
@@ -62,8 +62,8 @@ const Feature = () => {
         <Row className="justify-content-center">
           <Col lg={6} md={8}>
             <div className="title text-center mb-5">
-              <h3 className="font-weight-normal text-dark"><span className="text-warning">Benefits</span></h3>
-              <p className="text-muted">Lets try/p>
+              <h3 className="font-weight-normal text-dark"><span className="text-warning">Features</span></h3>
+              <p className="text-muted">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
             </div>
           </Col>
         </Row>
