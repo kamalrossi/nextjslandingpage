3783f07b8dfa69bce2c9b39cd8add19b6e832e42
Rossi Kamal <rossikamal@gmail.com>
2022-07-15 06:53:41 +0600

    Update About.js

--- components/About.js
+++ components/About.js
@@ -10,7 +10,7 @@ const About = () => {
         <Row className="justify-content-center">
           <Col lg={6} md={8}>
             <div className="title text-center mb-5">
-              <h3 className="font-weight-normal text-dark">Story <span className="text-warning">ElegantWallet</span></h3>
+              <h3 className="font-weight-normal text-dark">Story of <span className="text-warning">ElegantWallet</span></h3>
               <p className="text-muted">
                 ElegantWallet starts it journey with conventional app development in payment sectors. Now it has expanded into banking, utility and
                 marketplace software usecases
@@ -27,7 +27,7 @@ const About = () => {
             <Row>
               <Col md={6}>
                 <h6 className="text-dark font-weight-light f-20 mb-3">Services</h6>
-                <p className="text-muted font-weight-light">Banking, utilities and ecommerce are our service areas..Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type</p>
+                <p className="text-muted font-weight-light">Banking, utilities and ecommerce are our service areas.</p>
               </Col>
               <Col md={6}>
                 <h6 className="text-dark font-weight-light f-20 mb-3"> Business goal</h6>
