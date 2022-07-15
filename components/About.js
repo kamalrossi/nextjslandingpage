aca3a9994717bff587437d69b8c937787c2a78ab
Rossi Kamal <rossikamal@gmail.com>
2022-07-15 06:44:27 +0600

    Update About.js

--- components/About.js
+++ components/About.js
@@ -10,24 +10,28 @@ const About = () => {
         <Row className="justify-content-center">
           <Col lg={6} md={8}>
             <div className="title text-center mb-5">
-              <h3 className="font-weight-normal text-dark">About <span className="text-warning">Us</span></h3>
-              <p className="text-muted">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type</p>
+              <h3 className="font-weight-normal text-dark">Story <span className="text-warning">ElegantWallet</span></h3>
+              <p className="text-muted">
+                ElegantWallet starts it journey with conventional app development in payment sectors. Now it has expanded into banking, utility and
+                marketplace software usecases
+                
+                </p>
             </div>
           </Col>
         </Row>
         <Row>
           <Col md={4}>
-            <h2 className="font-weight-light line-height-1_6 text-dark mb-4">Lorem Ipsum has been the industry's standard dummy text</h2>
+            <h2 className="font-weight-light line-height-1_6 text-dark mb-4">No Hidden Charge, Enjoy pay on the go</h2>
           </Col>
           <Col md={{ size:7, offset :1 }}>
             <Row>
               <Col md={6}>
-                <h6 className="text-dark font-weight-light f-20 mb-3">Our Mission</h6>
-                <p className="text-muted font-weight-light">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type</p>
+                <h6 className="text-dark font-weight-light f-20 mb-3">Services</h6>
+                <p className="text-muted font-weight-light">Banking, utilities and ecommerce are our service areas..Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type</p>
               </Col>
               <Col md={6}>
-                <h6 className="text-dark font-weight-light f-20 mb-3">Our Vision</h6>
-                <p className="text-muted font-weight-light">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type</p>
+                <h6 className="text-dark font-weight-light f-20 mb-3"> Business goal</h6>
+                <p className="text-muted font-weight-light">We care to save money of consumers</p>
               </Col>
             </Row>
           </Col>
